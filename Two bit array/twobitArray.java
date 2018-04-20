d02afadb69b58304ade480b7bf4cd035632f55a8
MAlj11 <37505583+MAlj11@users.noreply.github.com>
2018-04-20 10:50:49 +0800

    Update twobitArray.java

--- Two bit array/twobitArray.java
+++ Two bit array/twobitArray.java
@@ -1,12 +1,12 @@
 /**
- * ��һ����ά�����У�ÿһ�ж����մ����ҵ�����˳������ÿһ�ж����մ��ϵ��µ�����˳������
- * �����һ������������������һ����ά�����һ���������ж��������Ƿ��и�������
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  * 
  * @author MA
  * 
  */
 
-// The method is bad..
+// 这个算法只是实现了功能，但是并不好
 public class twobitArray {
 	public static boolean lookupNum(int[][] arr, int targetNum) {
 	
