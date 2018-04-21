9179c12c0586cb0734fc3595a5def1d5aaec3c36
MAlj11 <malijun1114@outlook.com>
2018-04-21 17:01:36 +0800

    bubblesort

---
+++ bubble sort/bubbleSort.java
@@ -0,0 +1,18 @@
+﻿/**
+ * 冒泡排序
+ * @author MA
+ *
+ */
+public class bubbleSort {
+      public static void sort(int[] array){
+    	  for(int i=0;i<array.length-1;i++){
+    		  for(int j=1;j<array.length-1-i;j++){
+    			  if(array[j]>array[j+1]){
+    				  int temp = array[j];
+    				  array[j+1] = array[j];
+    				  array[j+1] = temp;
+    			  }
+    		  }
+    	  }
+      }
+}
